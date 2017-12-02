8c64ff6e160adba6569df9eeb892b103eff16585
Jackson McLaughlin <jacksonmclaughlin1@gmail.com>
2017-12-01 19:34:21 -0800

    DisableRobot Command

---
+++ src/org/usfirst/frc/team7171/robot/commands/DisableRobot.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team7171.robot.commands;
+
+import org.usfirst.frc.team7171.robot.Robot;
+import org.usfirst.frc.team7171.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DisableRobot extends Command {
+	
+	private DriveTrain dt;
+	public DisableRobot() {
+		dt = Robot.dt;
+	}
+
+	@Override
+	protected void initialize() {
+		requires(dt);
+		setInterruptible(false);
+		dt.setPower(0, 0);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
