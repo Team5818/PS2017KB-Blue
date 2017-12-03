0ef9a6b18dfa3bce8412ecc9b5f3fd7cc2aac27e
Jackson McLaughlin <jacksonmclaughlin1@gmail.com>
2017-12-03 14:44:40 -0800

    shooter subsystem

---
+++ src/org/usfirst/frc/team7171/robot/subsystems/Shooter.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team7171.robot.subsystems;
+
+
+import org.usfirst.frc.team7171.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Shooter extends Subsystem {
+
+	public static final boolean EXTENDED = true;
+	public static final boolean RETRACTED = !EXTENDED;
+
+	private Solenoid plunger;
+	
+	public Shooter() {
+		plunger = new Solenoid(RobotMap.SHOOTER);
+	}
+
+	public void setPlunger(boolean extended) {
+		plunger.set(extended);
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+
+	}
+
+}
