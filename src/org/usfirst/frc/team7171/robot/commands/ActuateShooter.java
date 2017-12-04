64ee05dcaf2fb21368a809090667e89eccec7b97
Jackson McLaughlin <jacksonmclaughlin1@gmail.com>
2017-12-04 11:36:07 -0800

    refactored Shoot to go both ways

--- src/org/usfirst/frc/team7171/robot/commands/ActuateShooter.java
+++ src/org/usfirst/frc/team7171/robot/commands/ActuateShooter.java
@@ -5,19 +5,30 @@ import org.usfirst.frc.team7171.robot.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-public class Shoot extends Command {
+public class ActuateShooter extends Command {
 
 	private Shooter shooter;
+	private boolean retract;
+
 	private static final double TIMEOUT = 0.5;
-	public Shoot() {
+	public ActuateShooter(boolean retract) {
+		this.retract = retract;
 		shooter = Robot.shooter;
 		requires(shooter);
 		setTimeout(TIMEOUT);
 	}
+	
+	public ActuateShooter() {
+		this(false);
+	}
 
 	@Override
 	public synchronized void start() {
-		shooter.setPlunger(shooter.EXTENDED);
+		if (retract) {
+			shooter.setPlunger(shooter.RETRACTED);
+		} else {
+			shooter.setPlunger(shooter.EXTENDED);
+		}
 	}
 
 	@Override
